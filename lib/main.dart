b3c5ba48c3f6718432b2eb83bf5b0a45ce20faad
Purvi <rawoolpurvi01@gmail.com>
2026-02-10 07:26:25 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flash_card/screens/welcome_screen.dart';
+import 'package:flash_card/screens/login_screen.dart';
+import 'package:flash_card/screens/registration_screen.dart';
+import 'package:flash_card/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: "/",
+      routes: {
+        '/': (context) => WelcomeScreen(),
+        '/login': (context) => LoginScreen(),
+        '/registration': (context) => RegistrationScreen(),
+        '/chat': (context) => ChatScreen(),
+      },
+    );
+  }
+}
